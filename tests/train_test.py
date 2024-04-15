a4e905f2bade821df2c6c4ff400ec681cbccfe59
Roman Novak <romann@google.com>
2024-04-15 16:03:58 +0000

    No public description
    PiperOrigin-RevId: 624978817
    Change-Id: I0ddb959477ae76e170180ff9a7007c9298a3f552

--- tests/train_test.py
+++ tests/train_test.py
@@ -114,21 +114,21 @@ class TrainTest(parameterized.TestCase):
     self.assertEqual(state.step, 1)
 
     reference = {
-        "__train_loss": 3.9817634,
-        "train_loss": 3.9817634,
+        "__train_loss": 3.945808,
+        "train_loss": 3.945808,
         "train_ntokens": 124,
 
-        "grads/all/rms": 0.01179235,
-        "grads/all/mean": 3.3595228e-05,
-        "grads/all/std": 0.0117923,
+        "grads/all/rms": 0.01341779,
+        "grads/all/mean": 3.026796e-05,
+        "grads/all/std": 0.01341776,
 
-        "updates/all/rms": 0.9997482,
-        "updates/all/mean": 0.002217,
-        "updates/all/std": 0.9997457,
+        "updates/all/rms": 0.99979043,
+        "updates/all/mean": -0.00446726,
+        "updates/all/std": 0.9997804,
 
-        "params/all/rms": 0.13636664,
-        "params/all/mean": 0.00515785,
-        "params/all/std": 0.13626905,
+        "params/all/rms": 0.16059065,
+        "params/all/mean": 0.00510256,
+        "params/all/std": 0.16050959,
 
         "learning_rate": c.opt.init_learning_rate,
 
